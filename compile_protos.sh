4570d614e3af9c92f8e5edf180b679371e397f3e
Rob Shakir <robjs@google.com>
2022-10-13 10:20:06 -0700

    Check in a log protobuf that can be used for replays.
     * (A) compile_proto.sh
       - Script to compile protobufs.
     * (A) go.{mod,sum}
       - Go module.
     * (A) proto/log/...
       - Protobuf definition for a log that currently just contains
         gRPC binary log entries.

---
+++ compile_protos.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Copyright 2022 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Go
+proto_imports_go=".:${GOPATH}/src"
+protoc -I="$proto_imports_go" --go_out=. --go_opt=paths=source_relative proto/log/log.proto
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/openconfig/replayer
+
+go 1.16
+
+require (
+	google.golang.org/grpc v1.50.0
+	google.golang.org/protobuf v1.28.1
+)
